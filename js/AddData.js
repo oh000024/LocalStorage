3bdc4bc7317add8d8da8e2d894556bd93739838f
JakeOhcs <Jakeoh@JakeOhs-MacBook-Pro.local>
2016-11-14 09:44:19 -0500

    [Mod] Editing Data

--- js/AddData.js
+++ js/AddData.js
@@ -1,44 +1,72 @@
-//
-//      <li class="contact">
-//        <span class="delete"></span>
-//        <h3>Poe Dameron</h3>
-//        <p class="email">poe.dameron@resistance.org</p>
-//        <p class="phone">613-842-6363</p>
-//      </li>
+function addContact(name, email, phone) {
 
-function addContact(name, email,phone)
-{
-	name = "Jake Oh";
-	email = "abcf@gmail.com";
-	phone = "12341234-q1234";
-	
 	let ul = document.querySelector("ul");
 	let li = document.createElement("li");
 	li.classList.add("contact");
 
-	
+
 	let span = document.createElement("span");
 	span.classList.add("delete");
-	
+
 	let h3 = document.createElement("h3");
-	h3.textContent="Jake Oh";
-	
+	h3.textContent = name;
+
 	let pEmail = document.createElement("p");
 	pEmail.classList.add("email");
-	pEmail.textContent= email;
-	
+	pEmail.textContent = email;
+
 	let pPhone = document.createElement("p");
 	pPhone.classList.add("phone");
-	pPhone.textContent=phone;
-	
-	
+	pPhone.textContent = phone;
+
+
 	li.appendChild(span);
 	li.appendChild(h3);
 	li.appendChild(pEmail);
 	li.appendChild(pPhone);
-	
+
 	ul.appendChild(li);
+
+}
+
+function clearNode() {
+	let ul = document.querySelector("ul");
+	ul.innerHTML="";
+
+}
+
+
+
+function addContact(contact) {
+	let ul = document.querySelector("ul");
+	let li = document.createElement("li");
+	li.classList.add("contact");
 	
-	
-	
+
+	let span = document.createElement("span");
+	span.classList.add("delete");
+	span.addEventListener('click', deleteData);
+
+	let h3 = document.createElement("h3");
+	h3.textContent = contact.fullname;
+	h3.addEventListener('click',editData);
+
+	let pEmail = document.createElement("p");
+	pEmail.classList.add("email");
+	pEmail.textContent = contact.email;
+	pEmail.addEventListener('click',editData);
+
+	let pPhone = document.createElement("p");
+	pPhone.classList.add("phone");
+	pPhone.textContent = contact.phone;
+	pPhone.addEventListener('click',editData);
+
+
+	li.appendChild(span);
+	li.appendChild(h3);
+	li.appendChild(pEmail);
+	li.appendChild(pPhone);
+
+	ul.appendChild(li);
+
 }
--- js/main.js
+++ js/main.js
@@ -1,75 +1,109 @@
-function overlay() {
-	el = document.getElementsByClassName("overlay");
-	el[0].style.visibility = (el[0].style.visibility == "visible") ? "hidden" : "visible";
-}
 
-var contacts = JSON.parse(data);
+
+const GOBALKEY = "oh000024";
 
 document.addEventListener("DOMContentLoaded", Init);
-let contactList = [];
+let contactList = []; //new MAP():
 
-function addData() {
-	//	addContact("jake oh","jakeohcs@gmail.com","asdf-asdf");
-	//	
-	//	var modaldiv = document.createElement("div");
-	//	modaldiv.classList.add("modal");
+function searchData(fullname, email, phone) {
+	contactList.find(function (item) {
 
-	//clear out local storage
-	//localStorage.clear();	
-	//localStorage.removeItem("oh000024");
+		if((item.fullname===fullname) && ( item.email === email) && (item.phone === phone)){
+			return item;
+		}
+		else
+			return nul;
+	});
+}
 
-
-
-	// OpenModal();
-	overlay();
-
-	//InsertData();
-
-	//InputData();
-
+function saveData() {
+	let namevalue = document.getElementById("fullname");
+	let emailvalue = document.getElementById("email");
+	let phonevalue = document.getElementById("phone");
 
 	let contact1 = {
-		fullname: "Jake Oh",
-		email: "ohddd@gg.com",
-		phne: "234234"
+		fullname: namevalue.value,
+		email: emailvalue.value,
+		phone: phonevalue.value
 	};
 
-
 	contactList.push(contact1);
-	console.log(contactList);
-
-	//localStorage.setItem("oh000024", JSON.stringify(constactList));
-};
-
-function deleteData(event) {
-
-	removeData;
-
-};
-
-function Init() {
-	var btFab = document.getElementsByClassName("fab")[0].addEventListener('click', addData);
-	//var btDelete = document.getElementsByClassName("delete")[0].addEventListener('click', deleteData);
-
-
-	if (!localStorage.getItem("oh000024")) {
-		console.log("No contact data: createing content");
-
-		//		let contact1 = {
-		//			fullname: "Jake Oh",
-		//			email: "ohddd@gg.com",
-		//			phne: "234234"
-		//		};
-
-		//		contactList.push(contact1);
-		//		localStorage.setItem("oh000024", JSON.stringify(contactList));
-	}
-
+	archiveData();
+	flipInputModal();
 	displayContact();
 }
 
-function displayContact() {
-	contactList = JSON.parse(localStorage.getItem("oh000024"));
+function cancel() {
+	flipInputModal();
+}
 
+function deleteData(ev) {
+
+	let name = ev.currentTarget.parentElement.childNodes[1].textContent;
+
+	for (let i = 0; i < contactList.length; i++) {
+		if (contactList[i].fullname === name) {
+			contactList.splice(i, 1);
+			break;
+		}
+	}
+	archiveData();
+	displayContact();
+};
+
+function editData(ev) {
+	flipInputModal();
+	//	console.log(ev.parentElement());
+	var elementChildren = ev.target.parentNode.children;
+
+	console.trace(elementChildren);
+	document.getElementById("fullname").value = elementChildren[1].textContent;
+	document.getElementById("email").value = elementChildren[2].textContent;
+	document.getElementById("phone").value = elementChildren[3].textContent;
+	
+	
+}
+
+function flipInputModal() {
+
+	el = document.getElementsByClassName("overlay");
+	el[0].style.visibility = (el[0].style.visibility == "visible") ? "hidden" : "visible";
+};
+
+
+function Init() {
+	if (!localStorage.getItem(GOBALKEY)) {
+		console.log("No contact data: createing content");
+
+		let contact1 = {
+			fullname: "Jake Oh",
+			email: "Jakeoh@gotmail.com",
+			phone: "787-9098"
+		};
+		contactList.push(contact1);
+		archiveData();
+	}
+	displayContact();
+
+}
+
+function archiveData() {
+	localStorage.setItem(GOBALKEY, JSON.stringify(contactList));
+}
+
+function displayContact() {
+
+	clearNode();
+	contactList = JSON.parse(localStorage.getItem(GOBALKEY));
+	
+	contactList.sort(function compare(a, b) {
+		if (a.fullname < b.fullname)
+			return -1;
+		else if (a.fullname === b.fullname)
+			return 0;
+		return 1;
+	});
+
+	contactList.forEach(addContact);
 	console.log(contactList);
 }
