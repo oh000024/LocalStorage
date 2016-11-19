d2c45fdfc574e6737e0a854def9a8e8d0222928a
JakeOhcs <Jakeoh@JakeOhs-MacBook-Pro.local>
2016-11-19 01:43:13 -0500

    [Mod] Working

--- js/main.js
+++ js/main.js
@@ -1,58 +1,17 @@
-const GOBALKEY = "oh000024";
-const NOT_FOUND_DATA = -1;
 document.addEventListener("DOMContentLoaded", Init);
 let contactList = []; //new MAP():
 
-function sortContact() {
-	contactList.sort(function compare(a, b) {
-		if (a.fullname < b.fullname)
-			return -1;
-		else if (a.fullname === b.fullname)
-			return 0;
-		return 1;
-	});
-}
-
-function searchData(fullname, email, phone) {
-	contactList.find(function (item) {
-
-		if ((item.fullname === fullname) && (item.email === email) && (item.phone === phone)) {
-			return item;
-
-		} else
-			return null;
-	});
-}
-
-function searchData(contactvalue) {
-	for (let i = 0; i < contactList.length; i++) {
-		if ((contactList[i].fullname == contactvalue.fullname) && (contactList[i].email == contactvalue.email) && (contactList[i].phone == contactvalue.phone)) {
-			return {
-				contact: contactList[i],
-				index: i
-			};
-		}
-	}
-	return {
-		contact: null,
-		index: -1
-
-	}
-}
 
 function saveData() {
 
-	let namevalue = document.getElementById("fullname");
-	let emailvalue = document.getElementById("email");
-	let phonevalue = document.getElementById("phone");
-
 	let contact1 = {
-		fullname: namevalue.value,
-		email: emailvalue.value,
-		phone: phonevalue.value
+		fullname: document.getElementById("fullname").value,
+		email: document.getElementById("email").value,
+		phone: document.getElementById("phone").value
 	};
 
 	let ret = searchData(contact1);
+
 	if (-1 != ret.index) {
 		contactList.splice(ret.index, 1);
 
@@ -60,28 +19,40 @@ function saveData() {
 		contactList.push(contact1);
 	}
 
+	updateAll();
+	offModal();
+}
 
-	displayContact();
-	archiveData();
-	flipInputModal();
+function editData(index, contact) {
+	contactList[index].fullname = document.getElementById("fullname").value;
+	contactList[index].email = document.getElementById("email").value;
+	contactList[index].phone = document.getElementById("phone").value;
 
+	updateAll();
+	offModal();
+
+	document.querySelector(".buttonSave").removeEventListener("click", editData);
+
+	document.querySelector(".buttonSave").onclick = function () {
+		saveData();
+	}
 }
 
 function cancel() {
-	flipInputModal();
+	offModal();
 }
 
 function addContact() {
 
-	flipInputModal();
-	let btnsave = document.querySelectorAll("input[type=button]");
-	btnsave[0].addEventListener('click', saveData);
-	btnsave[1].addEventListener('click', cancel);
-	let btnsave = document.querySelectorAll("input[type=button]");
-	//btnsave[0].addEventListener('click',editData);
+	let fullname = document.getElementById("fullname").value = "";
+	let email = document.getElementById("email").value = "";
+	let phone = document.getElementById("phone").value = "";
 
-//	btnsave[0].removeEventListener('click',editData);
-//	btnsave[0].addEventListener('click', saveData);	
+	let fun = function () {
+		saveData()
+	};
+
+	onModal();
 }
 
 function deleteContact(ev) {
@@ -113,45 +84,34 @@ function deleteContact(ev) {
 	}
 };
 
-function editData(index,contact) {
-	console.log(index);
-
-}
-
 function editContact(ev) {
-
 	let nValue = ev.currentTarget.parentElement.childNodes[1].textContent;
 	let eValue = ev.currentTarget.parentElement.childNodes[2].textContent;
 	let pValue = ev.currentTarget.parentElement.childNodes[3].textContent;
 
-	let obj = {
+	let li = ev.currentTarget.parentElement;
+
+	let orgobj = {
 		fullname: nValue,
 		email: eValue,
 		phone: pValue
 	};
 
-	let retContact = searchData(obj);
-
+	let retContact = searchData(orgobj);
+	//setting data in modal
 	if (NOT_FOUND_DATA != retContact.index) {
 		document.getElementById("fullname").value = contactList[retContact.index].fullname;
 		document.getElementById("email").value = contactList[retContact.index].email;
 		document.getElementById("phone").value = contactList[retContact.index].phone;
 	}
 
-	let btnsave = document.querySelectorAll("input[type=button]");
-	//btnsave[0].addEventListener('click',editData);
-
-//	btnsave[0].removeEventListener('click',saveData);
-//	btnsave[0].addEventListener('click', editData);
-	flipInputModal();
+	onModal();
+	//	var fun = editData(retContact.index,obj);
+	document.querySelector(".buttonSave").removeEventListener("click", saveData);
+	document.querySelector(".buttonSave").onclick = function () {
+		editData(retContact.index, orgobj);
+	}
 }
-
-function flipInputModal() {
-
-	el = document.querySelector(".overlay");
-	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
-};
-
 
 function Init() {
 	if (!localStorage.getItem(GOBALKEY)) {
@@ -168,17 +128,7 @@ function Init() {
 
 	contactList.pop();
 	contactList = JSON.parse(localStorage.getItem(GOBALKEY));
+
 	setBtnEvent();
-
-}
-
-function archiveData() {
-	localStorage.setItem(GOBALKEY, JSON.stringify(contactList));
-}
-
-function displayContact() {
-
-	clearNode();
-	sortContact();
-	contactList.forEach(addChildNode);
+	displayContact();
 }
---
+++ js/util.js
@@ -0,0 +1,172 @@
+const GOBALKEY = "oh000024";
+const NOT_FOUND_DATA = -1;
+const SAVEMODE = 1;
+const EDITMODE = 2;
+
+function sortContact() {
+	contactList.sort(function compare(a, b) {
+		if (a.fullname < b.fullname)
+			return -1;
+		else if (a.fullname === b.fullname)
+			return 0;
+		return 1;
+	});
+}
+
+function searchData(contactvalue) {
+	for (let i = 0; i < contactList.length; i++) {
+		if ((contactList[i].fullname == contactvalue.fullname) && (contactList[i].email == contactvalue.email) && (contactList[i].phone == contactvalue.phone)) {
+			return {
+				contact: contactList[i],
+				index: i
+			};
+		}
+	}
+	return {
+		contact: null,
+		index: -1
+
+	}
+}
+
+function updateAll() {
+	archiveData();
+	displayContact();
+
+}
+
+//function initReadData(name, email, phone) {
+//
+//	let ul = document.querySelector("ul");
+//	let li = document.createElement("li");
+//	li.classList.add("contact");
+//
+//
+//	let span = document.createElement("span");
+//	span.classList.add("delete");
+//
+//	let h3 = document.createElement("h3");
+//	h3.textContent = name;
+//
+//	let pEmail = document.createElement("p");
+//	pEmail.classList.add("email");
+//	pEmail.textContent = email;
+//
+//	let pPhone = document.createElement("p");
+//	pPhone.classList.add("phone");
+//	pPhone.textContent = phone;
+//
+//
+//	li.appendChild(span);
+//	li.appendChild(h3);
+//	li.appendChild(pEmail);
+//	li.appendChild(pPhone);
+//
+//	ul.appendChild(li);
+//
+//}
+
+function clearNode() {
+	let ul = document.querySelector("ul");
+	ul.innerHTML = "";
+
+}
+//
+//function initReadData(contact) {
+//	let ul = document.querySelector("ul");
+//	let li = document.createElement("li");
+//	li.classList.add("contact");
+//
+//
+//	let span = document.createElement("span");
+//	span.classList.add("delete");
+//	span.addEventListener('click', deleteContact);
+//
+//	let h3 = document.createElement("h3");
+//	h3.textContent = contact.fullname;
+//	h3.addEventListener('click', editContact);
+//
+//	let pEmail = document.createElement("p");
+//	pEmail.classList.add("email");
+//	pEmail.textContent = contact.email;
+//	pEmail.addEventListener('click', editContact);
+//
+//	let pPhone = document.createElement("p");
+//	pPhone.classList.add("phone");
+//	pPhone.textContent = contact.phone;
+//	pPhone.addEventListener('click', editContact);
+//
+//	li.appendChild(span);
+//	li.appendChild(h3);
+//	li.appendChild(pEmail);
+//	li.appendChild(pPhone);
+//
+//	ul.appendChild(li);
+//
+//}
+function archiveData() {
+	localStorage.setItem(GOBALKEY, JSON.stringify(contactList));
+}
+
+function displayContact() {
+
+	clearNode();
+	sortContact();
+	contactList.forEach(addChildNode);
+}
+
+function offModal() {
+	document.querySelector(".overlay").style.display = "none";
+	document.querySelector(".modal").style.display = "none";
+};
+
+function onModal() {
+	document.querySelector(".overlay").style.display = "block";
+	document.querySelector(".modal").style.display = "block";
+}
+
+function addChildNode(contact) {
+	let ul = document.querySelector("ul");
+	let li = document.createElement("li");
+	li.classList.add("contact");
+
+
+	let span = document.createElement("span");
+	span.classList.add("delete");
+	span.addEventListener('click', deleteContact);
+
+	let h3 = document.createElement("h3");
+	h3.textContent = contact.fullname;
+	h3.addEventListener('click', editContact);
+
+	let pEmail = document.createElement("p");
+	pEmail.classList.add("email");
+	pEmail.textContent = contact.email;
+	pEmail.addEventListener('click', editContact);
+
+	let pPhone = document.createElement("p");
+	pPhone.classList.add("phone");
+	pPhone.textContent = contact.phone;
+	pPhone.addEventListener('click', editContact);
+
+	li.appendChild(span);
+	li.appendChild(h3);
+	li.appendChild(pEmail);
+	li.appendChild(pPhone);
+
+	ul.appendChild(li);
+}
+
+function setBtnEvent() {
+	//	let fab = document.querySelector(".fab");
+	//	fab.addEventListener('click', addContact);
+	//	displayContact();
+	let btn = document.querySelector(".buttonSave");
+	btn.addEventListener("click", saveData);
+	document.querySelector(".buttonCancel").addEventListener("click", cancel);
+	document.querySelector(".fab").addEventListener("click", addContact);
+	//	let btnsave = document.querySelectorAll("input[type=button]");
+	//	btnsave[0].addEventListener('click', saveData);
+	//	btnsave[1].addEventListener('click', cancel);
+
+}
